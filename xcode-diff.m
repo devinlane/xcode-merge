eee36d39d40fb0cad4794982306dad9e709a04aa
Devin Lane <devin@shiftedbits.org>
2009-08-06 22:02:37 -0700

    initial working version

---
+++ xcode-diff.m
@@ -0,0 +1,319 @@
+#import <Foundation/Foundation.h>
+
+typedef struct {
+    /* The dictionary of objects for base, local, and remote */
+    NSDictionary *base;
+    NSDictionary *local;
+    NSDictionary *remote;
+    
+    /* The merged object dictionary */
+    NSMutableDictionary *merged;
+    
+    BOOL interactive;
+} Context;
+
+Context ctx;
+
+NSArray *uuid_keys_for_isa(NSString *isa)
+{
+    NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                [NSArray arrayWithObjects:@"fileRef", nil], @"PBXBuildFile", 
+                                [NSArray arrayWithObjects:@"containerPortal", nil], @"PBXContainerItemProxy", 
+                                [NSArray arrayWithObjects:@"files", nil], @"PBXCopyFilesBuildPhase", 
+                                [NSArray arrayWithObjects:@"files", nil], @"PBXFrameworksBuildPhase", 
+                                [NSArray arrayWithObjects:@"files", nil], @"PBXResourcesBuildPhase", 
+                                [NSArray arrayWithObjects:@"children", nil], @"PBXGroup", 
+                                [NSArray arrayWithObjects:@"buildConfigurationList", @"buildPhases", @"buildRules", @"dependencies", @"productReference", nil], @"PBXNativeTarget", 
+                                [NSArray arrayWithObjects:@"buildConfigurationList", @"mainGroup", @"targets", nil], @"PBXProject", 
+                                [NSArray arrayWithObjects:@"files", nil], @"PBXShellScriptBuildPhase", 
+                                [NSArray arrayWithObjects:@"files", nil], @"PBXSourcesBuildPhase", 
+                                [NSArray arrayWithObjects:@"target", @"targetProxy", nil], @"PBXTargetDependency", 
+                                [NSArray arrayWithObjects:@"children", nil], @"PBXVariantGroup", 
+                                [NSArray arrayWithObjects:@"buildConfigurations", nil], @"XCConfigurationList", nil];
+    
+    return [dictionary objectForKey:isa];
+}
+
+id merge_conflict(NSString *key, id base, id local, id remote, id context, BOOL *fail)
+{
+    printf("Merge conflict on key: %s base: %s\n", [[key description] UTF8String], [[base description] UTF8String]);
+    printf("Left: %s\nRight: %s\n", [[local description] UTF8String], [[remote description] UTF8String]);
+    
+    char res = '\0';
+    
+    for (;;) {
+        printf("Choose (l) left, (r) right, (c) context, (a) abort: ");
+        fflush(stdin);
+        if (fscanf(stdin, "%c", &res) == 1) {
+            if (tolower(res) == 'l') {
+                return local;
+            } else if (tolower(res) == 'r') {
+                return remote;
+            } else if (tolower(res) == 'a') {
+                *fail = YES;
+                break;
+            } else if (tolower(res) == 'c') {
+                printf("\n%s\n", [[context description] UTF8String]);
+            }
+        }
+    }
+    
+    return nil;
+}
+
+id element_merge3(NSString *key, id base, id local, id remote, id context, BOOL *fail);
+
+NSDictionary *dictionary_merge3(NSDictionary *base, NSDictionary *local, NSDictionary *remote, BOOL *fail)
+{
+    /* A real merge! Gather all keys and perform a recursive merge. */
+    NSMutableDictionary *merged = [NSMutableDictionary dictionary];
+    NSMutableSet *allKeys = [NSMutableSet set];
+    [allKeys addObjectsFromArray:[base allKeys]];
+    [allKeys addObjectsFromArray:[local allKeys]];
+    [allKeys addObjectsFromArray:[remote allKeys]];
+
+    for (NSString *key in allKeys) {
+        id bValue = [base objectForKey:key];
+        id lValue = [local objectForKey:key];
+        id rValue = [remote objectForKey:key];
+        
+        id result = element_merge3(key, bValue, lValue, rValue, (base ?: (local ?: remote)), fail);
+        
+        if (result) {
+            [merged setObject:result forKey:key];
+        }
+        
+        if (*fail) break;
+    }
+    
+    return merged;
+}
+
+NSArray *array_merge3(NSArray *base, NSArray *local, NSArray *remote, BOOL *fail)
+{
+    NSMutableArray *merged = [NSMutableArray array];
+    NSMutableSet *allItems = [NSMutableSet set];
+    [allItems addObjectsFromArray:base];
+    [allItems addObjectsFromArray:local];
+    [allItems addObjectsFromArray:remote];
+    
+    for (id item in allItems) {
+        id bItem = [base containsObject:item] ? item : nil;
+        id lItem = [local containsObject:item] ? item : nil;
+        id rItem = [remote containsObject:item] ? item : nil;
+        
+        id result = element_merge3(nil, bItem, lItem, rItem, nil, fail);
+        if (result) {
+            [merged addObject:result];
+        }
+        
+        if (*fail) break;
+    }
+    
+    return merged;    
+}
+
+id element_merge3(NSString *key, id base, id local, id remote, id context, BOOL *fail)
+{
+    *fail = NO;
+    
+    id result = nil;
+    
+    if (base && !(local || remote)) {
+        /* The entry exists in base, but neither remote or local (deleted) */
+        result = nil;
+    } else if (!base && (local || remote) && !(local && remote)) {
+        /* Added on one side only */
+        result = (local ?: remote);
+    } else if (base && (local || remote) && !(local && remote)) {
+        /* Existed in the base, and currently on one side, but not the other. 
+         * If the element in local or remote is equal to the one on the base, delete it.
+         * If it has changed, ask the user which to keep */
+        if ([base isEqual:(local ?: remote)]) {
+            result = nil;
+        } else {
+            result = merge_conflict(key, base, local, remote, context, fail);
+        }
+    } else if (local && remote) {
+        /* Two separate changes (potentially) */
+        /* Something is different! */
+        if ([local isKindOfClass:[NSArray class]]) {
+            return array_merge3(base, local, remote, fail);
+        } else if ([local isKindOfClass:[NSDictionary class]]) {
+            return dictionary_merge3(base, local, remote, fail);
+        } else if ([local isEqual:remote]) {
+            /* Identical! */
+            return local;            
+        } else {
+            /* Merge conflict */
+            return merge_conflict(key, base, local, remote, context, fail);
+        }
+    }
+    
+    return result;
+}
+
+NSDictionary *objects_merge3(NSDictionary *base, NSDictionary *local, NSDictionary *remote)
+{
+    NSMutableDictionary *mergedObjects = [NSMutableDictionary dictionary];
+    
+    /* First, get aggregate set of keys */
+    NSMutableSet *allKeys = [NSMutableSet set];
+    [allKeys addObjectsFromArray:[base allKeys]];
+    [allKeys addObjectsFromArray:[local allKeys]];
+    [allKeys addObjectsFromArray:[remote allKeys]];
+    
+    /* Iterate over all keys */
+    for (NSString *key in allKeys) {
+        BOOL fail = NO;
+        NSDictionary *result = element_merge3(key, [base objectForKey:key], [local objectForKey:key], [remote objectForKey:key], nil, &fail);
+        
+        /* If we didn't resolve a conflict */
+        if (fail) return nil;
+        
+        if (result) {
+            /* Merge in new entries */
+            [mergedObjects setObject:result forKey:key];
+        }
+    }
+    
+    return mergedObjects;
+}
+
+NSDictionary *project_file_merge3(NSDictionary *base, NSDictionary *local, NSDictionary *remote)
+{
+    NSMutableDictionary *document = [NSMutableDictionary dictionaryWithDictionary:base];
+    
+    /* Merge elements */
+    NSDictionary *mergedObjects = objects_merge3([base objectForKey:@"objects"], [local objectForKey:@"objects"], [remote objectForKey:@"objects"]);
+    if (!mergedObjects) return nil;
+    
+    /* Put the elements in our document */
+    [document setObject:mergedObjects forKey:@"objects"];
+    
+    /* Find project element and enforce root object */
+    NSDictionary *project = [[[mergedObjects allValues] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"isa = %@", @"PBXProject"]] objectAtIndex:0];
+    [document setObject:[[mergedObjects allKeysForObject:project] objectAtIndex:0] forKey:@"rootObject"];
+    
+    return document;
+}
+
+void serialize_element(id element, NSMutableString *string, NSUInteger level)
+{
+    if ([element isKindOfClass:[NSArray class]]) {
+        NSString *levelString = [@"" stringByPaddingToLength:level + 1 withString:@"\t" startingAtIndex:0];
+        [string appendString:@"(\n"];
+        for (id item in element) {
+            [string appendString:levelString];
+            serialize_element(item, string, level + 1);
+            [string appendString:@",\n"];
+        }
+        [string appendFormat:@"%@)", [@"" stringByPaddingToLength:level withString:@"\t" startingAtIndex:0]];
+    } else if ([element isKindOfClass:[NSDictionary class]]) {
+        NSString *levelString = [@"" stringByPaddingToLength:level + 1 withString:@"\t" startingAtIndex:0];
+        [string appendString:@"{\n"];
+        for (id key in [(NSDictionary *)element allKeys]) {
+            [string appendFormat:@"%@%@ = ", levelString, key];
+            serialize_element([(NSDictionary *)element objectForKey:key], string, level + 1);
+            [string appendString:@";\n"];
+        }
+        [string appendFormat:@"%@}", [@"" stringByPaddingToLength:level withString:@"\t" startingAtIndex:0]];
+    } else if ([element isKindOfClass:[NSString class]]) {
+        /* Escape string */
+        NSMutableString *escapedString = [[element mutableCopy] autorelease];
+        NSArray *escapableCharacters = [NSArray arrayWithObjects:@"\n", @"\"", @"\t", nil];
+        NSArray *escapes = [NSArray arrayWithObjects:@"\\n", @"\\\"", @"\\t", nil];
+        for (NSUInteger i = 0; i < [escapableCharacters count]; i++) {
+            [escapedString replaceOccurrencesOfString:[escapableCharacters objectAtIndex:i] withString:[escapes objectAtIndex:i] options:0 range:NSMakeRange(0,[escapedString length])];
+        }
+        
+        if ([escapedString rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@" -<>(){}+?"]].length != 0 ||
+            [escapedString length] == 0) {
+            [string appendFormat:@"\"%@\"", escapedString];
+        } else {
+            [string appendString:escapedString];
+        }
+    } else {
+        [string appendString:[element description]];
+    }
+}
+
+NSData *serialize_ascii_plist(NSDictionary *base)
+{
+    NSMutableString *string = [NSMutableString string];
+    [string appendString:@"// !$*UTF8*$!\n"];
+    
+    serialize_element(base, string, 0);
+    
+    return [string dataUsingEncoding:NSUTF8StringEncoding];
+}
+
+#if 0
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    NSString *error;
+    NSDictionary *dict = [NSPropertyListSerialization propertyListFromData:[NSData dataWithContentsOfFile:@"/project.LOCAL.pbxproj"] 
+                                                          mutabilityOption:NSPropertyListImmutable 
+                                                                    format:NULL 
+                                                          errorDescription:&error];
+    NSLog(@"first: %@", error);
+    
+    /* Write the merged data */
+    NSData *data = serialize_ascii_plist(dict);
+    
+    [data writeToFile:@"/Volumes/CC/Things mac/Things.xcodeproj/project.pbxproj" atomically:YES];
+    
+    
+    
+    dict = [NSPropertyListSerialization propertyListFromData:[NSData dataWithContentsOfFile:@"/Volumes/CC/Things mac/Things.xcodeproj/project.pbxproj"] 
+                                                          mutabilityOption:NSPropertyListImmutable 
+                                                                    format:NULL 
+                                                          errorDescription:&error];
+    
+    NSLog(@"%@", error);
+    
+    [pool drain];
+    return 0;
+}
+
+#endif
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    /* Input is %O %A %B %M. %O is the base, %A is the left and %B is the right. 
+     * We are expected to overwrite A with the results, unless %M is defined, in
+     * which case we write the results to %M. */
+    
+    if (argc <= 3) {
+        printf("Usage: xcode-diff <base> <local> <remote> [<merge>]\n");
+        exit(1);
+    }
+    
+    /* Load dictionaries */
+    NSDictionary *dicts[3];
+    for (NSUInteger i = 0; i < 3; i++) {
+        NSString *path = [NSString stringWithUTF8String:argv[i + 1]];
+        dicts[i] = [NSPropertyListSerialization propertyListFromData:[NSData dataWithContentsOfFile:path] 
+                                                    mutabilityOption:NSPropertyListImmutable 
+                                                              format:NULL 
+                                                    errorDescription:NULL];
+    }
+    
+    
+    /* Do the merge */
+    NSDictionary *result = project_file_merge3(dicts[0], dicts[1], dicts[2]);
+    
+    /* If we failed, exit with a non-zero return */
+    if (!result) return 1;
+    
+    /* Write the merged data */
+    NSData *data = serialize_ascii_plist(result);
+    
+    NSString *destination = [NSString stringWithUTF8String:(argc > 4 ? argv[4] : argv[2])];
+    [data writeToFile:destination atomically:YES];
+    
+    [pool drain];
+    return 0;
+}
